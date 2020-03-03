b93ac80011c82db5b3861bd6a7aab7e88990774e
VeLoct84 <unlimited84man@gmail.com>
2020-03-04 06:00:44 +0800

    modified Readme files and canvas

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# (FST)
-This app is creating folder sutructure tempelate.
+## [FST] ###### Folder Structure Template
+
+> ### OverView
+>
+> - This script is creating folder sutructure tempelate. It's still
+>   work in progress.
+
+> ### Instruction
+>
+> - This script is running in Python 3.7 and above. Please refer requiment.txt
+>   as for requirement.
+> - Run python script __init__.py
+
+Any suggestions can email
+*unlimited84man@gmail.com*
--- canvas.py
+++ canvas.py
@@ -1,16 +1,48 @@
+#!/usr/bin/python3.7
+
+"""
+OVERVIEW:
+    This app intended to be run in Windows 10. This app function to generate
+    folder structure and sub-folder.
+"""
 import tkinter as tk
 
-
+# Title of the interface
 window = tk.Tk()
-window.title("Template Folder")
-frame_a = tk.Frame(width=400, height=190)
+window.title("Template Folder Structure")
+frm_a = tk.Frame(width=400, height=150)
+frm_b = tk.Frame(width=400, height=25, bg="grey")
 
-label_a = tk.Label(master=frame_a, text="Save as path: ")
-label_a.pack()
+frm_a.pack()
+frm_b.pack(fill=tk.X)
 
-entry = tk.Entry(frame_a, text="Please fill the path")
-entry.place(x=100, y=25)
+# Set layout post
+lbl_path = tk.Label(master=frm_a, text="Save as path: ")
+lbl_path.pack()
+lbl_path.place(x=10, y=25)
 
-frame_a.pack()
+# Set entry for input
+ent_path = tk.Entry(master=frm_a, width=40, borderwidth=1)
+ent_path.pack()
+ent_path.place(x=80, y=25)
+# Insert sample path:
+ent_path.insert(0, "../folderName/filesRevit")
 
+btn_generate = tk.Button(master=frm_a, text="Generate Folder")
+btn_generate.place(x=80, y=50)
+window.mainloop()
+
+# Version and Credit.
+"""
+Fix bugs of the text version and credit
+"""
+lbl_desc = tk.Label(
+    master=frm_b,
+    text="Beta Version 03/20\t Develops by:Lokman",
+    fg="blue",
+    bg="grey"
+)
+lbl_desc.pack()
+
+# Keep the app running
 window.mainloop()
