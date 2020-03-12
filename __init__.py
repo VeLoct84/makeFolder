e2b98eddabd6d70e4f8660d9e283db96c52cf3d7
VeLoct84 <unlimited84man@gmail.com>
2020-03-12 12:13:24 +0800

    move all the function into __init__files.

--- __init__.py
+++ __init__.py
@@ -5,35 +5,43 @@ OVERVIEW:
     This app intended to be run in Windows 10. This app function to generate
     folder structure and sub-folder.
 """
+
 import tkinter as tk
 import pathlib
-import test
+import main
+
+
+# Create function
+def create_folders(root_path):
+    for folder in main.mainFolder:
+        (root_path / folder).mkdir(parents=True, exist_ok=True)
+       
+def main():
+    root = tk.get() 
+    root_path = pathlib.Path(root)
+    create_folders(root_path)
+
+if __name__ == "__main__":
+    main()
 
 # Title of the interface
 window = tk.Tk()
 window.title("Template Folder Structure")
-frm_a = tk.Frame(master=window, width=400, height=150)
-
-frm_a.pack()
-
+frame = tk.Frame(master=window)
 # Set layout post
-lbl_path = tk.Label(master=frm_a, text="Save as path: ")
-lbl_path.pack()
-lbl_path.place(x=10, y=25)
+lbl_path = tk.Label(master=window, text="Save as path: ")
+lbl_path.grid(row=0, column=0, padx=5, pady=5, sticky='w')
 
 # Set entry for input
-ent_path = tk.Entry(master=frm_a, width=40, borderwidth=1)
-root_path = pathlib.Path(ent_path)
-
-ent_path.pack()
-ent_path.place(x=80, y=25)
+ent_path = tk.Entry(master=window, width=40, borderwidth=1)
+ent_path.grid(row=0, column=1, padx=5, pady=5, sticky='e')
 
 # create button to interact the function
-btn_generate = tk.Button(master=frm_a, text="Generate Folder")
-btn_generate.place(x=80, y=50)
+btn_generate = tk.Button(
+    master=window, text="Generate Folder", command=create_folders
+)
+btn_generate.grid(row=1, column=1, padx=5, pady=5, sticky='e')
 
-# function run when click
-btn_generate.bind("<Button-1>", test.create_folders)
 
 # Keep the app running
 window.mainloop()
